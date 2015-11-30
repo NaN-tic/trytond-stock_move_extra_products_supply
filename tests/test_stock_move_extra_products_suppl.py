710b1913e7fb77d551ce0dd44a0bdf343ee32503
resteve <resteve@zikzakmedia.com>
2015-12-01 00:35:39 +0100

    Tests

--- tests/test_stock_move_extra_products_suppl.py
+++ tests/test_stock_move_extra_products_suppl.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the stock_move_extra_products_supply module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockMoveExtraProductsSupplyTestCase(unittest.TestCase):
+class StockMoveExtraProductsSupplyTestCase(ModuleTestCase):
     'Test Stock Move Extra Products Supply module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_move_extra_products_supply')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_move_extra_products_supply')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_move_extra_products_supply'
 
 
 def suite():
